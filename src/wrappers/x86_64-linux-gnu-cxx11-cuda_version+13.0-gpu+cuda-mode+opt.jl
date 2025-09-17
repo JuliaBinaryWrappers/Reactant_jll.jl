3083d5aec6f11a829bc05f5e13509015a490efda
jlbuild <juliabuildbot@gmail.com>
2025-09-17 00:55:01 +0000

    Reactant_jll build 0.0.240+0

--- src/wrappers/x86_64-linux-gnu-cxx11-cuda_version+13.0-gpu+cuda-mode+opt.jl
+++ src/wrappers/x86_64-linux-gnu-cxx11-cuda_version+13.0-gpu+cuda-mode+opt.jl
@@ -22,11 +22,11 @@ JLLWrappers.@declare_library_product(libnccl, "libnccl.so.2")
 JLLWrappers.@declare_library_product(libnvJitLink, "libnvJitLink.so.13")
 JLLWrappers.@declare_library_product(libnvrtc, "libnvrtc.so.13")
 JLLWrappers.@declare_library_product(libnvrtc_builtins, "libnvrtc-builtins.so.13.0")
-JLLWrappers.@declare_file_product(libnvshmem_device)
-JLLWrappers.@declare_library_product(libnvshmem_host, "libnvshmem_host.so.3")
 JLLWrappers.@declare_library_product(nvshmem_bootstrap_uid, "nvshmem_bootstrap_uid.so.3")
 JLLWrappers.@declare_library_product(nvshmem_transport_ibrc, "nvshmem_transport_ibrc.so.3")
 JLLWrappers.@declare_executable_product(fatbinary)
+JLLWrappers.@declare_file_product(libnvshmem_device)
+JLLWrappers.@declare_library_product(libnvshmem_host, "libnvshmem_host.so.3")
 JLLWrappers.@declare_executable_product(ptxas)
 function __init__()
     JLLWrappers.@generate_init_header()
@@ -149,17 +149,6 @@ function __init__()
         nothing,
     )
 
-    JLLWrappers.@init_file_product(
-        libnvshmem_device,
-        "lib/libnvshmem_device.bc",
-    )
-
-    JLLWrappers.@init_library_product(
-        libnvshmem_host,
-        "lib/libnvshmem_host.so.3",
-        nothing,
-    )
-
     JLLWrappers.@init_library_product(
         nvshmem_bootstrap_uid,
         "lib/nvshmem_bootstrap_uid.so",
@@ -175,6 +164,17 @@ function __init__()
     JLLWrappers.@init_executable_product(
         fatbinary,
         "lib/cuda/bin/fatbinary",
+    )
+
+    JLLWrappers.@init_file_product(
+        libnvshmem_device,
+        "lib/libnvshmem_device.bc",
+    )
+
+    JLLWrappers.@init_library_product(
+        libnvshmem_host,
+        "lib/libnvshmem_host.so.3",
+        nothing,
     )
 
     JLLWrappers.@init_executable_product(
